3ebb6cfe3bcad02d47f9bb192858ed9dff920d8a
Selamawit123 <selae.welde@gmail.com>
2021-06-02 07:48:14 -0500

    Ass7

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # CS445-lab solutions
 1.[Lab 1](./lab1/lab1.md)
 1.[Lab 2](./lab2/lab2.md)
-<<<<<<< HEAD
-1. [Lab4](Lab4/Lab4.md)
-  
-=======
+1. [Lab 4](.lab4/lab4.md)
+1. [Lab 7](.lab7/lab7.md)
+
 
   
--- lab4/ascii.js
+++ lab4/ascii.js
@@ -1,10 +1,10 @@
-"use strict"
-window.onload = function() {
-    document.getElementById("start_button").onclick = fTimer;
-    document.getElementById("stop_button").onclick = stopAnimation;
-    document.getElementById("animation").onchange = animation;
-    document.getElementById("fontsize").onchange = fontSize;
-    document.getElementById("checkbox").onchange = turbo;
+"use strict";
+window.onload = function () {
+  document.getElementById("start_button").onclick = fTimer;
+  document.getElementById("stop_button").onclick = stopAnimation;
+  document.getElementById("animation").onchange = animation;
+  document.getElementById("fontsize").onchange = fontSize;
+  document.getElementById("checkbox").onchange = turbo;
 };
 
 var animate = [];
@@ -12,60 +12,55 @@ var timer = null;
 var i = 0;
 var frame = null;
 function animation() {
-    document.getElementById("start_button").disabled = false;
-    var animation = document.getElementById("animation").value;
-    animate = ANIMATIONS[animation].split("=====\n");
+  document.getElementById("start_button").disabled = false;
+  var animation = document.getElementById("animation").value;
+  animate = ANIMATIONS[animation].split("=====\n");
 
-    frame = ANIMATIONS[animation];
+  frame = ANIMATIONS[animation];
 
-    document.getElementById("text-area").value = ANIMATIONS[animation];
-
+  document.getElementById("text-area").value = ANIMATIONS[animation];
 }
 
 function fTimer() {
-    if (timer == null) {
-        timer = setInterval(start, 250);
-    }
+  if (timer == null) {
+    timer = setInterval(start, 250);
+  }
 }
 function fontSize() {
-    var text = document.getElementById("text-area");
-    text.style.fontSize = document.getElementById("fontsize").value + "pt";
+  var text = document.getElementById("text-area");
+  text.style.fontSize = document.getElementById("fontsize").value + "pt";
 }
 function startAnimation() {
-    document.getElementById("text-area").value = animate[i];
-    if (i < animate.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    document.getElementById("stop_button").disabled = false;
-    document.getElementById("start_button").disabled = true;
-    document.getElementById("animation").disabled = true;       
-    document.getElementById("checkbox").disabled = false;    
-
-
+  document.getElementById("text-area").value = animate[i];
+  if (i < animate.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  document.getElementById("stop_button").disabled = false;
+  document.getElementById("start_button").disabled = true;
+  document.getElementById("animation").disabled = true;
+  document.getElementById("checkbox").disabled = false;
 }
 
 function stopAnimation() {
-    clearInterval(timer);
-    document.getElementById("stop_button").disabled = true;
-    document.getElementById("start_button").disabled = false;
-    ocument.getElementById("animation").disabled = false; 
-    document.getElementById("text-area").value = frame;
+  clearInterval(timer);
+  document.getElementById("stop_button").disabled = true;
+  document.getElementById("start_button").disabled = false;
+  ocument.getElementById("animation").disabled = false;
+  document.getElementById("text-area").value = frame;
 
-   
-    timer = null;
-   
-    i = 0;
+  timer = null;
+
+  i = 0;
 }
 
 function turbo() {
-   
-        let isTurbo = document.getElementById("checkbox").checked;
-        clearInterval(timer);
-        if (isTurbo){
-            timer = setInterval(start, 50);
-        }else{
-            timer = setInterval(start, 250);      
-        }
-    }
+  let isTurbo = document.getElementById("checkbox").checked;
+  clearInterval(timer);
+  if (isTurbo) {
+    timer = setInterval(start, 50);
+  } else {
+    timer = setInterval(start, 250);
+  }
+}
